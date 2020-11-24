8e916f10d869448b651fd72c7b0ceb1c0e6fdac1
Alex Stewart <alex.stewart14361@gmail.com>
2020-11-24 14:11:59 +1300

    BUG. fixed bug where window resize event caused dragging function to not work properly.

--- modules/dragNode.js
+++ modules/dragNode.js
@@ -1,6 +1,5 @@
 export function dragElements(nodes) {
     // get boundaires of parent div
-    const parentSize = document.getElementById('nodes-div').getBoundingClientRect()
     for(let i = 0; i < nodes.length; i++){      
         //get the node from the array
         let currentNode = nodes[i]
@@ -20,6 +19,9 @@ export function dragElements(nodes) {
             e = e || window.event
             e.preventDefault()
     
+            //get the size of the parent div when the element is clicked
+            const parentSize = document.getElementById('nodes-div').getBoundingClientRect()
+
             //get the mouse position
             var mouseX = e.clientX
             var mouseY = e.clientY
@@ -29,8 +31,8 @@ export function dragElements(nodes) {
             const toRight = mouseX > 25
             const toLeft = mouseX < parentSize.width -25
 
-            let newY = ((mouseY)/parentSize.height)*100-26
-            let newX = ((mouseX)/parentSize.width)*100-2
+            let newY = (mouseY/parentSize.height)*100-26
+            let newX = (mouseX/parentSize.width)*100-2
 
             if( belowTop && aboveBottom ){
                 element.style.top = `${newY}%`
@@ -46,12 +48,11 @@ export function dragElements(nodes) {
                 }
             }
         }
-    
+
         function closeDragElement() {
             // stop moving when mouse button is released:
             document.onmouseup = null
             document.onmousemove = null
         }
     }
-    return console.log("returning")
 }
