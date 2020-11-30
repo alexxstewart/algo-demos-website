ceed77b42e45c598cd4ff56d37bbe699e98241b6
Alex Stewart <alex.stewart14361@gmail.com>
2020-11-30 16:58:28 +1300

    FUNCTIONALITY. Line is animated dynamically now. Now needs dynamic length calculation.

--- modules/animations.js
+++ modules/animations.js
@@ -6,23 +6,95 @@ export async function animatePath(lines, path) {
     svg.setAttribute('width', document.getElementById('nodes-div').getBoundingClientRect().width)
     svg.setAttribute('id', 'line-container')
     document.getElementById('nodes-div').appendChild(svg)
-    
+
+    let linesNum = 0
+    let svgPath = []
+
     for(let i = 0; i < path.length-1; i++){
         for(let j = 0; j < lines.length; j++){
             let lineFrom = path[i]
             let lineTo = path[i+1]
             let currentLine = lines[j]
             if(currentLine.nodeA.number == lineFrom && currentLine.nodeB.number == lineTo){
-                addSVGLine(currentLine)
+                console.log(`Path from ${lineFrom} to ${lineTo}`)
+                if(linesNum == 0){
+                    // add both the first and last coords of the first line
+                    svgPath.push(currentLine.nodeA.x)
+                    svgPath.push(currentLine.nodeA.y)
+                }
+                svgPath.push(currentLine.nodeB.x)
+                svgPath.push(currentLine.nodeB.y)
+                linesNum++
             }else if(currentLine.nodeA.number == lineTo && currentLine.nodeB.number == lineFrom){
-                addSVGLine(currentLine)
+                console.log(`Path from ${lineFrom} to ${lineTo}`)
+                if(linesNum == 0){
+                    // add both the first and last coords of the first line
+                    svgPath.push(currentLine.nodeB.x)
+                    svgPath.push(currentLine.nodeB.y)
+                }
+                svgPath.push(currentLine.nodeA.x)
+                svgPath.push(currentLine.nodeA.y)
+                linesNum++
             }
         }
     }
+
+    animate(svgPath)
 }
 
-function addSVGLine(line){
+function animate(pathCoords){
+    
+    // we need to convert line coords into pixel values
+    const parentSize = document.getElementById('nodes-div').getBoundingClientRect()
+    for(let i = 0; i < pathCoords.length; i += 2){
+        pathCoords[i] = pathCoords[i]/100 * parentSize.width + 25;
+        pathCoords[i+1] = pathCoords[i+1]/100 * parentSize.height + 25;
+    }
 
+    // compute the line length to help with the animation
+    const lineLength = 10000//computeLine(length)
+
+    console.log(pathCoords)
+    let pathString = 'M'
+    pathString += `${pathCoords[0]} ${pathCoords[1]} `
+    for(let i = 2; i < pathCoords.length; i+=2){
+        console.log(pathCoords[i], pathCoords[i+1])
+        pathString += `L${pathCoords[i]} ${pathCoords[i+1]} `
+    }
+    //pathString += 'Z'
+    console.log(pathString)
+
+    // now we can create the path element and add the pathString as a d property
+    let pathElement = document.createElementNS('http://www.w3.org/2000/svg','path')
+    pathElement.setAttribute('d', pathString)
+
+    // add the new element to the svg container
+    document.getElementById('line-container').appendChild(pathElement)
+
+    const styleString = `
+        fill: none;
+        stroke-dasharray: ${lineLength};
+        stroke-dashoffset: ${lineLength};
+        animation: dash 5s linear forwards;
+        stroke-width: 20px;
+        stroke: red;
+    `;
+
+    pathElement.setAttribute('style', styleString)
+    /*
+    path {
+    fill: none;
+    stroke-dasharray: 1000;
+    stroke-dashoffset: 1000;
+    animation: dash 5s linear forwards;
+    stroke-width: 20px;
+    stroke: yellow;
+    }
+    */
+}
+
+async function addSVGLine(line, lineNumber){
+    console.log("Adding SVG line")
     // get the coords of the lines end points
     let nodeA_x = line.nodeA.x
     let nodeA_y = line.nodeA.y
@@ -38,7 +110,7 @@ function addSVGLine(line){
     nodeB_y = nodeB_y/100 * parentSize.height + shift
 
     // add the css animation
-    //cssAnimation()
+    cssAnimation(lineNumber)
 
     console.log(nodeA_x, nodeA_y, nodeB_x, nodeB_y)
     // create a line and add it to the svg element
@@ -51,6 +123,7 @@ function addSVGLine(line){
     newLine.setAttribute("stroke", "yellow")
     newLine.setAttribute('stroke-width', 15)
     document.getElementById('line-container').appendChild(newLine)
+    sleep(5000)
 }
 
 function sleep(ms) {
@@ -62,24 +135,14 @@ function sleep(ms) {
 ============================================================
 */
 
-function cssAnimation(){
+function cssAnimation(lineNum){
     var style = document.createElement('style');
     const lineLength = 2000
     var keyFrames = `
-    #line1 {
+    #line${lineNum} {
         stroke-dasharray: ${lineLength};
         stroke-dashoffset: ${lineLength};
         animation: dash 5s linear forwards;
-    }
-    @keyframes dash {
-        to {
-            stroke-dashoffset: 0;
-        }
-    }
-    @keyframes dash {
-        to {
-            stroke-dashoffset: 0;
-        }
     }`;
     document.getElementsByTagName('head')[0].appendChild(style);
 }
